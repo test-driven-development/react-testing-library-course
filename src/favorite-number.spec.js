2fc8a5aae8a8c6c9b0361d8ec5cc2cbb94a12f3a
wil pannell <wil.pannell@gmail.com>
2020-07-23 04:41:04 -0700

    04-learn-use-dom-testing-library-to-write-more-maintainable-react-tests

--- src/favorite-number.spec.js
+++ src/favorite-number.spec.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import ReactDom from 'react-dom'
+import {getQueriesForElement} from '@testing-library/react'
 // noinspection ES6PreferShortImport
 import {FavoriteNumber} from './favorite-number'
 
 test('renders', () => {
   const div = document.createElement('div')
   ReactDom.render(<FavoriteNumber />, div)
-  expect(div.querySelector('input')).toHaveAttribute('type', 'number')
-  expect(div.querySelector('label')).toHaveTextContent('Favorite Number')
+  const {getByLabelText} = getQueriesForElement(div)
+  // eslint-disable-next-line testing-library/prefer-screen-queries
+  const input = getByLabelText(/favorite number/i)
+  expect(input).toHaveAttribute('type', 'number')
 })
