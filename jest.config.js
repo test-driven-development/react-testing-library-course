d530d65408f5b8b9ed90ae2819465d472cc75e6d
wil pannell <wil.pannell@gmail.com>
2020-07-23 04:21:12 -0700

    reconfigure jest

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,11 @@ module.exports = {
   ...config,
   // we have no coverageThreshold on this project...
   coverageThreshold: {},
+  notify: true,
+  notifyMode: 'always',
+  verbose: true,
+  testMatch: [
+    '**/__tests__/**/*.+(js|jsx|ts|tsx)',
+    '**/+(*.)+(spec|test).+(js|jsx|ts|tsx)',
+  ],
 }
--- tests/setup-env.js
+++ tests/setup-env.js
@@ -1,10 +1,9 @@
 import '@testing-library/jest-dom/extend-expect'
 import 'jest-axe/extend-expect'
 import * as td from 'testdouble'
-import * as tdJest from 'testdouble-jest'
 
 global.td = td
-tdJest(td, jest)
+require('testdouble-jest')(td, jest)
 
 afterEach(() => {
   td.reset()
