d5cca42184dfe902d62b498aef165a0feeabda1a
wil pannell <wil.pannell@gmail.com>
2020-07-22 21:39:13 -0700

    introduce testdouble.js

--- tests/setup-env.js
+++ tests/setup-env.js
@@ -1,2 +1,11 @@
 import '@testing-library/jest-dom/extend-expect'
 import 'jest-axe/extend-expect'
+import * as td from 'testdouble'
+import * as tdJest from 'testdouble-jest'
+
+global.td = td
+tdJest(td, jest)
+
+afterEach(() => {
+  td.reset()
+})
