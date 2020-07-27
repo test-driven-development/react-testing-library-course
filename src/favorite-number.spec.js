5b48bc196fee2f2ae6e1fedcbf3fe534f97d95dd
wil pannell <wil.pannell@gmail.com>
2020-07-27 06:11:07 -0700

    05-scikit-learn-use-react-testing-library-to-render-and-test-react-components (extract helper)

--- src/favorite-number.spec.js
+++ src/favorite-number.spec.js
@@ -4,10 +4,15 @@ import {getQueriesForElement} from '@testing-library/react'
 // noinspection ES6PreferShortImport
 import {FavoriteNumber} from './favorite-number'
 
+function render(ui) {
+  const container = document.createElement('div')
+  ReactDom.render(ui, container)
+  const queries = getQueriesForElement(container)
+  return {container, ...queries}
+}
+
 test('renders', () => {
-  const div = document.createElement('div')
-  ReactDom.render(<FavoriteNumber />, div)
-  const {getByLabelText} = getQueriesForElement(div)
+  const {getByLabelText} = render(<FavoriteNumber />)
   // eslint-disable-next-line testing-library/prefer-screen-queries
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
