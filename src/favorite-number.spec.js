fa33651b6959c4a0d53e08bbe47f58acbba3c4c2
wil pannell <wil.pannell@gmail.com>
2020-07-27 06:16:02 -0700

    06-scikit-learn-debug-the-dom-state-during-tests-using-react-testing-library-s-debug-function

--- src/favorite-number.spec.js
+++ src/favorite-number.spec.js
@@ -4,8 +4,12 @@ import {render} from '@testing-library/react'
 import {FavoriteNumber} from './favorite-number'
 
 test('renders', () => {
-  const {getByLabelText} = render(<FavoriteNumber />)
+  const {getByLabelText, debug} = render(<FavoriteNumber />)
+  // eslint-disable-next-line testing-library/no-debug
+  debug()
   // eslint-disable-next-line testing-library/prefer-screen-queries
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
+  // eslint-disable-next-line testing-library/no-debug
+  debug(input)
 })
