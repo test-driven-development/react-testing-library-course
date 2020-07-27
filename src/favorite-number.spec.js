31a0fabe7c56615b4b18faa75316ef3b22692252
wil pannell <wil.pannell@gmail.com>
2020-07-27 06:24:26 -0700

    07-scikit-learn-test-react-component-event-handlers-with-fireevent-from-react-testing-library

--- src/favorite-number.spec.js
+++ src/favorite-number.spec.js
@@ -1,11 +1,18 @@
+/* eslint-disable testing-library/prefer-screen-queries */
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render, fireEvent} from '@testing-library/react'
 // noinspection ES6PreferShortImport
 import {FavoriteNumber} from './favorite-number'
 
+test('invalid value shows error message', () => {
+  const {getByLabelText, getByRole} = render(<FavoriteNumber />)
+  const input = getByLabelText(/favorite number/i)
+  fireEvent.change(input, {target: {value: '10'}})
+  expect(getByRole('alert')).toHaveTextContent(/the number is invalid/i)
+})
+
 test('renders', () => {
   const {getByLabelText} = render(<FavoriteNumber />)
-  // eslint-disable-next-line testing-library/prefer-screen-queries
   const input = getByLabelText(/favorite number/i)
   expect(input).toHaveAttribute('type', 'number')
 })
