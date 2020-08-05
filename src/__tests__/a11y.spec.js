c6c4dc004ce4a17a9d4792a60e62bb2fb84d3226
wil pannell <wil.pannell@gmail.com>
2020-08-05 10:42:54 -0700

    11-test-accessibility-of-rendered-react-components-with-jest-axe

---
+++ src/__tests__/a11y.spec.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import {axe} from 'jest-axe'
+
+function Form() {
+  return (
+    <form>
+      <label htmlFor="email">Email</label>
+      <input id="email" placeholder="email" />
+    </form>
+  )
+}
+
+test('inaccessible form', async () => {
+  const {container} = render(<Form />)
+  const results = await axe(container)
+  expect(results).toHaveNoViolations()
+})
