faabce8516bf3009bb9b14927b29decaafb0c108
wil pannell <wil.pannell@gmail.com>
2020-08-06 12:03:35 -0700

    17-ensure-error-boundaries-can-successfully-recover-from-errors

--- src/error-boundary.spec.js
+++ src/error-boundary.spec.js
@@ -1,31 +1,27 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {fireEvent, render} from '@testing-library/react'
 import {ErrorBoundary} from './error-boundary'
 import {reportError} from './api'
 
 jest.mock('./api')
-let spyInstance
-beforeAll(() => {
-  spyInstance = jest.spyOn(console, 'error').mockImplementation(() => {})
-})
 
-afterAll(() => {
-  spyInstance.mockRestore()
+beforeEach(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => {})
 })
 
 afterEach(() => {
+  console.error.mockRestore()
   jest.restoreAllMocks()
 })
 
-const dummyError = new Error('dummy')
-
 function ThrowError({shouldThrow}) {
-  if (shouldThrow) throw dummyError
+  if (shouldThrow) throw new Error('dummy')
+  return null
 }
 
 test('calls error boundary', () => {
   reportError.mockResolvedValueOnce({success: true})
-  const {rerender} = render(
+  const {rerender, getByText, getByRole, queryByRole, queryByText} = render(
     <ErrorBoundary>
       <ThrowError />
     </ErrorBoundary>,
@@ -40,4 +36,24 @@ test('calls error boundary', () => {
   expect(reportError).toHaveBeenCalledTimes(1)
   expect(reportError).toHaveBeenCalledWith(expect.any(Error), expect.anything())
   expect(console.error).toHaveBeenCalledTimes(2)
+
+  expect(getByRole('alert').textContent).toMatchInlineSnapshot(
+    `"There was a problem."`,
+  )
+
+  console.error.mockClear()
+  reportError.mockClear()
+
+  rerender(
+    <ErrorBoundary>
+      <ThrowError />
+    </ErrorBoundary>,
+  )
+
+  fireEvent.click(getByText(/try again/i))
+
+  expect(reportError).not.toHaveBeenCalled()
+  expect(console.error).not.toHaveBeenCalled()
+  expect(queryByRole('alert')).not.toBeInTheDocument()
+  expect(queryByText(/try again/i)).not.toBeInTheDocument()
 })
