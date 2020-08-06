e8bafd977502088ff97d99564e6eb0140a1e92e5
wil pannell <wil.pannell@gmail.com>
2020-08-06 00:56:05 -0700

    15-test-componentdidcatch-handler-error-boundaries-with-react-testing-library

---
+++ src/error-boundary.spec.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {render} from '@testing-library/react'
+import {ErrorBoundary} from './error-boundary'
+import {reportError} from './api'
+
+jest.mock('./api')
+afterEach(() => {
+  jest.restoreAllMocks()
+})
+
+const dummyError = new Error('dummy')
+
+function ThrowError({shouldThrow}) {
+  if (shouldThrow) throw dummyError
+}
+
+test('calls error boundary', () => {
+  reportError.mockResolvedValueOnce({success: true})
+  const {rerender} = render(
+    <ErrorBoundary>
+      <ThrowError />
+    </ErrorBoundary>,
+  )
+
+  rerender(
+    <ErrorBoundary>
+      <ThrowError shouldThrow={true} />
+    </ErrorBoundary>,
+  )
+
+  expect(reportError).toHaveBeenCalledTimes(1)
+  expect(reportError).toHaveBeenCalledWith(expect.any(Error), expect.anything())
+})
